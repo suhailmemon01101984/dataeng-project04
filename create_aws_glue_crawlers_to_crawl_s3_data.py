ffdb1036cad4eeeabf79ffaa45bfb5b2e776ef2c
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-01-06 15:30:14 -0700

    create_aws_glue_crawlers_to_crawl_s3_data.py

--- create_aws_glue_crawlers_to_crawl_s3_data.py
+++ create_aws_glue_crawlers_to_crawl_s3_data.py
@@ -22,7 +22,7 @@
 #4. go to aws console --> aws glue --> data catalog-->databases --> add database --> create the database: suhailmemon84-dev. this is the database
 # which the crawler will use to create the tables under after it's crawling is complete
 #5. once you run this code, go to https://us-east-1.console.aws.amazon.com/glue/home?region=us-east-1#/v2/data-catalog/crawlers and monitor
-# your crawler and once it complets, go to aws athena --> query editor and verify under the database: suhailmemon84-dev to ensure your tables are created properly by the crawler
+# your crawler and once it completes, go to aws athena --> query editor and verify under the database: suhailmemon84-dev to ensure your tables are created properly by the crawler
 
 
 import boto3
